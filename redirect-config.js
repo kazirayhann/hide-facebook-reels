e1f4927c906e465f7770757d20c18b988daef9d6
apixcel <apixcel.sa@gmail.com>
2026-06-16 20:35:32 +0600

    Add Islamic redirect links

--- redirect-config.js
+++ redirect-config.js
@@ -1,5 +1,15 @@
 globalThis.HFR_REDIRECT_URLS = [
   "https://youtu.be/iC-2t-FEbvk?si=HFHhChO8FPSWI_mQ",
   "https://youtu.be/Yx6Zy2X7d2k?si=KKrb94DKQw4q8i9W",
-  "https://youtu.be/6KGbCT3PS7U?si=A8Uq_x8HI9v0GhBO"
+  "https://youtu.be/6KGbCT3PS7U?si=A8Uq_x8HI9v0GhBO",
+  "https://www.youtube.com/results?search_query=surah+yasin+beautiful+recitation",
+  "https://www.youtube.com/results?search_query=surah+ar+rahman+beautiful+recitation",
+  "https://www.youtube.com/results?search_query=surah+al+mulk+beautiful+recitation",
+  "https://www.youtube.com/results?search_query=surah+al+kahf+beautiful+recitation",
+  "https://www.youtube.com/results?search_query=surah+al+waqiah+beautiful+recitation",
+  "https://www.youtube.com/results?search_query=ayat+al+kursi+beautiful+recitation",
+  "https://www.youtube.com/results?search_query=dua+qunoot+beautiful+recitation",
+  "https://www.youtube.com/results?search_query=morning+adhkar+dua",
+  "https://www.youtube.com/results?search_query=evening+adhkar+dua",
+  "https://www.youtube.com/results?search_query=ruqyah+shariah+quran+recitation"
 ];
